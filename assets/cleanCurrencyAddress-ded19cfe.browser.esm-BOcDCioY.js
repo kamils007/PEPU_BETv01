cd2b26c87038873439e4e53d07d0cac209f925f7
kamils007 <kamils007@users.noreply.github.com>
2025-04-11 12:47:19 +0000

    deploy: 454ea843b131de5c182b9b3c07dcdffe1a515207

---
+++ assets/cleanCurrencyAddress-ded19cfe.browser.esm-BOcDCioY.js
@@ -0,0 +1,1 @@
+import{s as t,at as a}from"./index-BY4YEPXk.js";function n(r){return t(r)?a:r}export{n as c};
---
+++ assets/hasERC20Allowance-654a940b.browser.esm-S_b4VNJ_.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/index-BY4YEPXk.js","assets/index-CjCS5sN2.css"])))=>i.map(i=>d[i]);
+import{f as _,i as c}from"./index-BY4YEPXk.js";async function f(e,a,t){const n=e.getProvider(),r=(await _(async()=>{const{default:o}=await import("./index-BY4YEPXk.js").then(l=>l.eu);return{default:o}},__vite__mapDeps([0,1]))).default,s=new c(n,a,r,{},e.storage),i=await e.getSignerAddress(),d=e.address;return(await s.read("allowance",[i,d])).gte(t)}export{f as h};
